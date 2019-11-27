fcc8e2d735f1177c3c31c39d69c7957a7d76f171
Ben Milnes <benjamin.milnes@nagwa.com>
2019-11-27 13:29:28 +0000

    Various minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,19 @@ const path = require('path');
 module.exports = {
   entry: {
     index: "./src/index.js",
-    check: "./src/check.js"
+    check: "./src/check.js",
   },
   output: {
-    filename: "[name].main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /tests\.js$/,
+        use: 'mocha-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
